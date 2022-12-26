5e53ba58483c731efcc946301f5626ce1b07d052
prinssharma <102709676+prinssharma@users.noreply.github.com>
2022-12-27 00:18:08 +0530

    Add files via upload

--- First.java
+++ First.java
@@ -1,35 +1,23 @@
-import java.util.*;
-public class First {
-    public static void swap(int arr[], int i, int j)
-    {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-    public static void main(First[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter array size");
-            int n = sc.nextInt();
-            int arr[] = new int [n];
-            System.out.print("Enter array element : ");
-            for(int i = 0; i < arr.length; i++)
-            {
-                arr[i] = sc.nextInt();
-            }
-            System.out.print("Entered array elements are :  ");
-            for(int i = 0; i < arr.length; i++)
-            {
-                System.out.print(" "+arr[i]);
-            }
-            int two[] = arr;
-            swap(two, 0, 3);
-            System.out.println();
-            System.out.print("After swapping array elements are : ");
-            for(int i = 0; i < arr.length; i++)
-            {
-                System.out.print(" "+arr[i]);
-            }
-        }
+import java.io.FileInputStream;
 
+
+public class First{
+
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        
+        try{
+            System.out.println("2");
+            FileInputStream fis = new FileInputStream("d:/abc.txt");
+            System.out.println("3");
+        }
+        catch(Exception ae)
+        {
+            System.out.println("4");
+            System.out.println(ae);
+            System.out.println("5");
+        }
+        System.out.println("Hello java");
     }
 }
---
+++ second.java
@@ -0,0 +1,25 @@
+
+
+public class second {
+    
+    public static void main(String[] args) {
+        
+        try{
+            System.out.println("1");
+            int a = 100, b = 0, c;
+            System.out.println("2");
+            c = a / b;
+            System.out.println("3");
+        }
+        catch(ArithmeticException e)
+        {
+            e.printStackTrace();
+            System.out.println("4");
+            System.out.println(e);
+            System.out.println(e.toString());
+            System.out.println("5");
+            System.out.println(e.getMessage());
+        }
+        System.out.println("Hello World");
+    }
+}
