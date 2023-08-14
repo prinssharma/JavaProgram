03f0ba06c355e0ca2a3097851ee4f6a73acb0f6c
prinssharma <21mcmc33@uohyd.ac.in>
2023-08-14 18:48:40 +0530

    Swapped Array element

---
+++ SwapArray.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+class SwapArray{
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array ");
+
+        int s = sc.nextInt();
+        int arr[] = new int[s];
+        System.out.println("Enter array element ");
+
+        for(int i=0; i<s; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Given array : ");
+
+        for(int i=0; i<s; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
+        SwapArray obj = new SwapArray();
+        arr = obj.reverse(arr);
+        System.out.print("Swapped array : ");
+        System.out.print(Arrays.toString(arr));
+    }
+
+    public int[] swap(int[] arr, int start, int end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+
+        return arr;
+    }
+
+    public int[] reverse(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+        while(start<=end){
+            swap(arr, start, end);
+            start++;
+            end--;
+        }
+        return arr;
+    }
+
+}
