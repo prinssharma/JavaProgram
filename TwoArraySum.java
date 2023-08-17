e9e9d30858272f6156c04d234e26965e7ce66218
prinssharma <21mcmc33@uohyd.ac.in>
2023-08-17 11:22:38 +0530

    Sum of product of two array element diagonally

---
+++ TwoArraySum.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class TwoArraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter size of the array");
+        int n = sc.nextInt();
+
+        int[] arr1 = new int[n];
+        int[] arr2 = new int[n];
+
+        System.out.println("Enter array1 element");
+        for(int i=0; i<n; i++){
+            arr1[i] =sc.nextInt();
+        }
+        System.out.println("Enter array2 element");
+        for(int i=0; i<n; i++){
+            arr2[i] =sc.nextInt();
+        }
+
+        System.out.println("Array 1 : "+ Arrays.toString(arr1));
+        System.out.println("Array 2 : "+ Arrays.toString(arr2));
+        
+
+        int sum = 0;
+        for(int i = 0; i<arr1.length; i++){
+            n = n-1;
+            sum += arr1[i] * arr2[n];
+        }
+        System.out.print("Output = " + sum);
+    }
+}
