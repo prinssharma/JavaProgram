01e79543431772a057fa12ddf606bda6b2c66d02
prinssharma <21mcmc33@uohyd.ac.in>
2023-08-23 12:42:32 +0530

    Ceil of the number updated

--- CeilingOfNumber.java
+++ CeilingOfNumber.java
@@ -1,26 +1,36 @@
 public class CeilingOfNumber {
     public static void main(String[] args) {
         int[] arr = {2, 3, 5, 9, 14, 16, 18};
-        int target = 7;
+        int target = 13;
         int ceil = searchCeilingNumber(arr, target);
         System.out.println("Ceil value of "+target+" is : "+ceil);
     }
 
     static int searchCeilingNumber(int[] arr, int key){
-        int low = 0;
-        int high = arr.length - 1;
+        int low=0;
+        int high = arr.length -1;
+        boolean isAsc = arr[low] < arr[high];
         while(low <= high){
-            int mid = low + (high -low) / 2;
+            int mid = low +(high - low ) / 2;
             if(arr[mid] == key){
-                return arr[mid];
+                return mid;
             }
-            else if(arr[mid] < key){
-                low = mid + 1;
+           if(isAsc){
+            if(arr[mid] < key){
+                low = mid+1;
             }
             else{
-                high = mid - 1;
+                high = mid -1;
             }
-
+           }
+           else{
+            if(arr[mid] > key){
+                low = mid+1;
+            }
+            else{
+                high = mid -1;
+            }
+           }
         }
         return arr[low];
         
